5f6cd9ca282915703e28558f7a1a61767bc1ae15
fan <chengfan19890826@gmail.com>
2019-08-18 08:59:48 +0100

    checking column ambiguity

--- src/main/java/com/fan/ANTLR/core/ColumnListener.java
+++ src/main/java/com/fan/ANTLR/core/ColumnListener.java
@@ -24,6 +24,86 @@ public class ColumnListener extends MySqlParserBaseListener {
     this.username = username;
     this.password = password;
     this.tableSet = tableSet;
+    getRightColumns();
+  }
+
+  public void getErrorColumns(String[] actualColumns) {
+
+    for (int i = 0; i < actualColumns.length; i++) {
+      if(checkPrefix(actualColumns[i])) {
+        if(!this.rightColumnSet.contains(removePrefix(actualColumns[i]))) {
+          errorColumns.add(removePrefix(actualColumns[i]));
+        }
+      }else{
+        if(!this.rightColumnSet.contains(actualColumns[i]) ||
+          checkAmbiguity(actualColumns[i])){
+          errorColumns.add(actualColumns[i]);
+        }
+      }
+    }
+
+    File f = new File("../webapps/SQL/WEB-INF/resources/error/semanticError.json");
+    try (PrintWriter pw = new PrintWriter(f)) {
+      if (this.errorColumns.size() == 0) {
+        pw.print("{}");
+      } else {
+        System.out.println("*****");
+        pw.print("{");
+        for (int i = 0; i < this.errorColumns.size() - 1; i++) {
+          pw.print("\"" + (i + 1) + "\":" + "\"" + this.errorColumns.get(i) + "\",");
+        }
+        pw.print("\"" + this.errorColumns.size() + "\":" + "\"" +
+          this.errorColumns.get(this.errorColumns.size() - 1) + "\"}");
+      }
+    } catch (Exception e) {
+      System.out.println(e.toString());
+    }
+  }
+
+  @Override
+  public void enterFullColumnName(MySqlParser.FullColumnNameContext ctx) {
+    super.enterFullColumnName(ctx);
+    if (checkPrefix(ctx.getText())) {
+      if(checkAmbiguity(removePrefix(ctx.getText())) && !checkAmbiguity(ctx.getText())){
+        this.actualColumnSet.add(ctx.getText());
+      }else{
+        this.actualColumnSet.add(removePrefix(ctx.getText()));
+      }
+    } else {
+      this.actualColumnSet.add(ctx.getText());
+    }
+  }
+
+  private boolean checkPrefix(String columnName) {
+    for (int i = 0; i < columnName.length(); i++) {
+      if (columnName.charAt(i) == '.') {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private String removePrefix(String columnName) {
+    int indexStart = 0;
+    for (int i = 0; i < columnName.length(); i++) {
+      if (columnName.charAt(i) == '.') {
+        indexStart = (i + 1);
+        return columnName.substring(indexStart);
+      }
+    }
+    return columnName.substring(indexStart);
+  }
+
+  public String[] getActualColumnSet() {
+    String[] actualColumns = new String[this.actualColumnSet.size()];
+
+    System.out.println("Actual columns*");
+    for (int i = 0; i < this.actualColumnSet.size(); i++) {
+      actualColumns[i] = this.actualColumnSet.get(i);
+      System.out.println(this.actualColumnSet.get(i));
+    }
+    System.out.println("Actual columns*");
+    return actualColumns;
   }
 
   private void getRightColumnsForOneTable(String table) {
@@ -44,39 +124,29 @@ public class ColumnListener extends MySqlParserBaseListener {
   }
 
   private void getRightColumns() {
-    for(int i = 0; i < this.tableSet.size(); i++) {
+    System.out.println("right columns*");
+    for (int i = 0; i < this.tableSet.size(); i++) {
       getRightColumnsForOneTable(this.tableSet.get(i));
+      System.out.println(this.tableSet.get(i));
+    }
+    System.out.println("right columns*");
+  }
+
+  private boolean checkAmbiguity(String s) {
+    int cnt = 0;
+    for (int j = 0; j < rightColumnSet.size(); j++) {
+      if (rightColumnSet.get(j).equals(s)) {
+        cnt++;
+      }
+    }
+    if(cnt > 1) {
+      return true;
+    }else {
+      return false;
     }
   }
 
-  public void getErrorColumns(String[] actualColumns) {
-    getRightColumns();
-
-    for(int i = 0; i < actualColumns.length; i++) {
-      if(!this.rightColumnSet.contains(actualColumns[i])) {
-        System.out.println(actualColumns[i]);
-        errorColumns.add(actualColumns[i]);
-      }
-    }
-
-    File f = new File("../webapps/SQL/WEB-INF/resources/error/semanticError.json");
-    try (PrintWriter pw = new PrintWriter(f)){
-      if (this.errorColumns.size() == 0) {
-        pw.print("{}");
-      } else {
-        System.out.println("*****");
-        pw.print("{");
-        for (int i = 0; i < this.errorColumns.size() - 1; i++) {
-          pw.print("\"" + (i + 1) + "\":" + "\"" + this.errorColumns.get(i) + "\",");
-        }
-        pw.print("\"" + this.errorColumns.size() + "\":" + "\"" +
-          this.errorColumns.get(this.errorColumns.size() - 1) + "\"}");
-      }
-    } catch (Exception e) {
-      System.out.println(e.toString());
-    }
-  }
-
+  /*
   @Override
   public void enterSelectColumnElement(MySqlParser.SelectColumnElementContext ctx) {
     super.enterSelectColumnElement(ctx);
@@ -90,32 +160,5 @@ public class ColumnListener extends MySqlParserBaseListener {
     }
   }
 
-  private boolean checkPrefix(String columnName) {
-    for(int i = 0; i < columnName.length(); i++) {
-      if(columnName.charAt(i) == '.') {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private String removePrefix(String columnName) {
-    int indexStart = 0;
-    for(int i = 0; i < columnName.length(); i++) {
-      if(columnName.charAt(i) == '.') {
-        indexStart = (i + 1);
-        return columnName.substring(indexStart);
-      }
-    }
-    return columnName.substring(indexStart);
-  }
-
-  public String[] getActualColumnSet() {
-    String[] actualColumns = new String[this.actualColumnSet.size()];
-
-    for(int i = 0; i < this.actualColumnSet.size(); i++) {
-      actualColumns[i] = this.actualColumnSet.get(i);
-    }
-    return actualColumns;
-  }
+   */
 }
